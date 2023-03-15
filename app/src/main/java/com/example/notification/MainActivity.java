5c88200dda7f47981fb275aba92e6936aaf7ad62
Arijit2003 <arijitmodak2003@gmail.com>
2023-03-15 12:54:09 +0530

    Notification, Pending Intent

---
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.notification;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+
+import java.util.ResourceBundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static  final  String CHANNEL_ID="MESSAGE CHANNEL";
+    private static  final  int NOTIFICATION_ID=100;
+    private static  final  int PI_REQ_CODE=10;
+    NotificationManager notificationManager;
+    Notification notification;
+    ActivityResultLauncher<String> postNotificationPermission= registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+                @Override
+                public void onActivityResult(Boolean result) {
+                    if(result){
+                        notificationManager.notify(NOTIFICATION_ID,notification);
+                    }
+                    else{
+                        showAlertDialog("POST NOTIFICATION permission denied","Therefore we cannot throw notification");
+                    }
+                }
+            }
+    );
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Drawable
+        Drawable drawable= ResourcesCompat.getDrawable(getResources(),R.drawable.img,null);
+        BitmapDrawable bitmapDrawable=(BitmapDrawable) drawable;
+        Bitmap largeIcon=bitmapDrawable.getBitmap();
+
+        //Pending Intent
+        Intent nIntent=new Intent(getApplicationContext(),MainActivity.class);
+        nIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pi = PendingIntent.getActivity(this,PI_REQ_CODE,nIntent,PendingIntent.FLAG_IMMUTABLE|PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //Step1 Notification Manager
+        notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        //step2: Notification
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O) {
+            notification = new Notification.Builder(this)
+                    .setLargeIcon(largeIcon)
+                    .setSmallIcon(R.drawable.img)
+                    .setContentIntent(pi)
+                    .setOngoing(true)
+                    .setContentText("F Notification")
+                    .setSubText("First Notification Implementation")
+                    .setChannelId(CHANNEL_ID)
+                    .build();
+            notificationManager.createNotificationChannel(new NotificationChannel(CHANNEL_ID,"My Channel",NotificationManager.IMPORTANCE_HIGH));
+        }
+        else{
+            notification = new Notification.Builder(this)
+                    .setLargeIcon(largeIcon)
+                    .setSmallIcon(R.drawable.img)
+                    .setContentIntent(pi)
+                    .setOngoing(true)
+                    .setContentText("F Notification")
+                    .setSubText("First Notification Implementation")
+                    .build();
+        }
+        postNotificationPermission.launch(android.Manifest.permission.POST_NOTIFICATIONS);
+
+    }
+    private void showAlertDialog(String title,String message){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+        dialog.setTitle(title);
+        dialog.setMessage(message);
+        dialog.setPositiveButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.create();
+        dialog.show();
+    }
+}
